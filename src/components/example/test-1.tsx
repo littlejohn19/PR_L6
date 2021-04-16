4f46195c20b540506fdeddd16ef64a30976f900e
Bartłomiej Mirek <bwmirek@gmail.com>
2021-04-16 10:55:29 +0200

    lab6

---
+++ src/components/example/test-1.tsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export default class Test1 extends Component {
+    state = {
+        value: 0,
+    };
+
+    setNewValue = () => {
+        this.setState({value: 1});
+    };
+
+    render() {
+        return <div>
+            <p>{this.state.value}</p>
+            <button className="btn btn-secondary btn-sm" onClick={this.setNewValue}>Set Value</button>
+        </div>;
+    }
+}
